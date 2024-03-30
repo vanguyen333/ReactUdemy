11081e90261b7d4fa6eddcd898ec974fb4c6f5ce
vanguyen333 <vanguyen333@yahoo.com>
2024-03-29 22:16:09 -0400

    43 Making components resuable with Props - core concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import reactImg from './assets/react-core-concepts.png';
+import componentsImg from './assets/components.png';
+
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
-function genRandomInt(max) {
+const genRandomInt=(max)=> {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function Header() {
+const Header=()=> {
   const description =  reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
        {description} React concepts you will need for almost any app you are going to build!
@@ -17,12 +20,36 @@ function Header() {
   );
 }
 
-function App() {
+const CoreConcept=(props)=>{
+  return (
+    <li>
+      <img src={props.img} alt={props.alt} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
+const App=()=> {
   return (
     <div>
       <Header />
       <main>
+      <section id="core-concepts">
         <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcept 
+          title="Components" 
+          description="The core UI building block."
+          img={componentsImg}
+          />
+          <CoreConcept 
+          title="Props"/>
+          <CoreConcept/>
+          <CoreConcept/>
+        </ul>
+      </section>
+      
       </main>
     </div>
   );
