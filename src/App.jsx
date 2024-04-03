45d3a89fd4c2a46321d32c7897f788f7a71e1988
vanguyen333 <vanguyen333@yahoo.com>
2024-04-03 10:45:54 -0400

    54 Deriving and outputting data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
-
+import { EXAMPLES } from './data.js';
 function App() {
 //selectedTopic is the first element from the array which we get back from the useState
 // will be the current data snapshot(current state value provided by React, may change
 // if the component function is executed again) for this component execution cycle
 // setSelectedTopic:(state updating function): updates teh stored value &
 // tells React to re-execute the component function in which useState() was called
-  const [selectedTopic,setSelectedTopic]= useState('Please click a button')
+  const [selectedTopic,setSelectedTopic]= useState('components')
   const tabContent = "Please click a button";
   function handleSelect(selectedBtn) {
     // selectedButton => 'components','jsx','props','state'
@@ -48,7 +48,15 @@ console.log('App Component Executing/Rendering')
           <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
 
         </menu>
-       {selectedTopic}
+       <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+          {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+       </div>
       </section>
       </main>
     </div>
