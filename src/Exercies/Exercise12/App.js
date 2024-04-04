1c92a8b385b5c0e53157791dcad7609ff19612b4
vanguyen333 <vanguyen333@yahoo.com>
2024-04-04 11:09:19 -0400

    Exercise 12 Dynamic List Content

---
+++ src/Exercies/Exercise12/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Todo from './Todo'
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
+// don't change the Component name "App"
+export default function App() {
+    return(
+        <ul>
+        {DUMMY_TODOS.map((item)=> <Todo text={item}/>)}
+        </ul>
+        )
+  
+}
