5806239029a9d9887521e4f59db78718192b7acd
vanguyen333 <vanguyen333@yahoo.com>
2024-04-04 10:29:53 -0400

    57 Outputting  List Data Dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -36,8 +36,10 @@ console.log('App Component Executing/Rendering')
       <Header />
       <main>
       <section id="core-concepts">
-        <h2>Time to get started!</h2>
+        <h2>Core Concepts</h2>
+    
         <ul>
+          {CORE_CONCEPTS.map((conceptItem)=> <CoreConcept key={conceptItem.title} {...conceptItem}/>)}
           <CoreConcept 
           title={CORE_CONCEPTS[0].title} 
           description={CORE_CONCEPTS[0].description} 
